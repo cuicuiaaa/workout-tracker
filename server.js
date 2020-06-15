cf5a82906da0ca41de017fcaa33e08e98ea3daa2
Clare <cuicuiaaa0307@gmail.com>
2020-06-15 18:09:26 +1000

    add get all workouts routes and html routes

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require("express");
+const path = require("path");
 const mongoose = require("mongoose");
 
 const PORT = process.env.PORT || 3000;
 
 const app = express();
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
